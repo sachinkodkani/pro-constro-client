f24c2ddc428b9bce20e1723326f124fce258839f
sachinkodkani <sachin.kodkani@gmail.com>
2019-02-19 21:37:03 +0530

    Login screen initialization

--- react-native/ProConstro/src/features/login/LoginScreen.js
+++ react-native/ProConstro/src/features/login/LoginScreen.js
@@ -12,3 +12,24 @@ const Container = styled.View`
   background-color: ${props => props.theme.background.PRIMARY_BACKGROUND_COLOR};
 `;
 
+const HeaderText = styled.Text`
+  font-size: ${props => props.theme.base.FONT_SIZE_MASSIVE};
+  color: ${props => props.theme.colorOption.PRIMARY_FOREGROUND_COLOR};
+  font-family: ${props => props.theme.base.PRIMARY_FONT_FAMILY};
+`;
+
+const Body = styled.View`
+  flex: 1;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.background.PRIMARY_BACKGROUND_COLOR};
+`;
+
+const Footer = styled.View`
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: ${heightPercentageToDP('8%')};
+  background-color: ${props => props.theme.background.PRIMARY_BACKGROUND_COLOR};
+`;
